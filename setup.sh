106f47e9962b41c118a67879e61384827ec9ecba
Kevin Benton <kevin.benton@nefeli.io>
2019-11-12 15:30:27 +0000

    alter move command to deal with existing dir

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
 git submodule init
 git submodule update
 git submodule foreach git submodule init
 git submodule foreach git submodule update
 
-mv "$DIR" "${HOME}/.vim"
+mv -T "$DIR" "${HOME}/.vim"
 ln -s "${HOME}/.vim/.vimrc" "${HOME}/.vimrc"
